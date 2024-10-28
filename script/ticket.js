5fdcc88f1984281b154d94b9d01730d530b62c37
captainboogey <anik35-700@diu.edu.bd>
2024-10-29 02:15:07 +0600

    adding seats

--- script/ticket.js
+++ script/ticket.js
@@ -1,23 +1,67 @@
+document.addEventListener("click", names);
 
-
-
-document.addEventListener('click',names)
-
-
-function setColor(elementId){
-    const element =document.getElementById(elementId);
-    element.classList.add('bg-[#1DD100]')
+function setColor(elementId) {
+  const element = document.getElementById(elementId);
+  element.classList.add("bg-[#1DD100]");
 }
 
-function names(event){
-    
-    const innText = event.target.firstChild.nodeValue;
-    
-   setColor(innText)
+function names(event) {
+  const innText = event.target.firstChild.nodeValue;
 
+  // console.log(innText)
+
+  setColor(innText);
 }
 
-// function clicked(){
-//     const textIn = names();
-//     setColor(textIn)
-// }
+document.addEventListener("click", remove);
+
+function remove(event) {
+  const innText = event.target.firstChild.nodeValue;
+  
+  let count = 0;
+  let button = document.getElementById("seat");
+  
+  button.addEventListener("click", function () {
+  const counts = count++;
+  const number = parseInt(counts);
+ 
+  console.log(number);
+   if(counts>2 || counts>3||counts>4||counts>5||counts>6){
+    removeBackground(innText)
+    
+    
+   }
+  
+  
+   
+    
+
+  
+});
+
+
+console.log(innText);
+}
+
+let count = 0;
+let button = document.getElementById("seat");
+let countDisplay = document.getElementById("count");
+let formSeat = document.getElementById("form-seat");
+
+button.addEventListener("click", function () {
+  count++;
+
+  let sum = 40;
+  sum = sum - count;
+  // console.log(sum);
+  countDisplay.innerHTML = sum;
+  formSeat.innerHTML = count;
+  if (count > 4) {
+    formSeat.innerHTML = 0;
+  }
+});
+function removeBackground(elementId) {
+  const element = document.getElementById(elementId);
+  element.classList.remove("bg-[#1DD100]");
+  console.log(element.classList)
+}
